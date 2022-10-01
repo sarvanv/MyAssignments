68388de5b0c6786ffdf37a72f7da9595e04a8473
sarvanv <114814799+sarvanv@users.noreply.github.com>
2022-10-01 10:37:00 -0400

    Week1-day1

---
+++ week1/day1/car.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class car {
+	boolean automatic=false;
+	byte milege=20;
+	int a=5;
+	char model='a';
+	short year=2022;
+	long engine=1234567l;
+	float modelno=123.456f;
+	double type=12345.67890;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ week1/day2/MyFirstjavaProgram.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class MyFirstjavaProgram {
+public static void main(String[] args) {
+	System.out.println("MyFirstJava Program");
+	for(int i=1;i<=10;i++)
+	{
+		System.out.println("welcome" + "Saravana"+ i);
+	}
+	
+		
+}
+}
